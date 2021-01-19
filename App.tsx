2ea6d1ffc0f0464604dbf2a24a63881382be6a4c
Filipe Câncio <filipe.cancio@outlook.com>
2021-01-19 19:20:21 -0300

    adição de fonte dosis e app loagina

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 
 import React from 'react';
 import Login from './src/pages/Login';
+import { AppLoading } from 'expo';
+import { useFonts,Dosis_400Regular,Dosis_700Bold } from '@expo-google-fonts/dosis';
 
 export default function App() {
-  return(
-    <>
-      <Login/>
-    </>
-  );
+  let [fontsLoaded] = useFonts({
+    Dosis_400Regular,
+    Dosis_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />;
+  }else{
+    return <Login />;
+  }
 }
 
